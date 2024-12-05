089a720f4c155d9d4e33a8b295842e2fb76538ec
davibgare <davidfelipegarciaortiz@gmail.comm>
2024-12-05 15:28:54 -0500

    Create Repository

---
+++ src/main/java/com/SIstemaPost/SistemaPost/SistemaPostApplication.java
@@ -0,0 +1,13 @@
+package com.SIstemaPost.SistemaPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPostApplication.class, args);
+	}
+
+}
